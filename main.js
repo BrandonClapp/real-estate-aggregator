ef05e98fc6fdf87caf5888f47a77d9794309f940
Brandon Clapp <clapp09@gmail.com>
2016-02-10 21:41:25 -0600

    now support for multiple sources

--- main.js
+++ main.js
@@ -1,72 +1,85 @@
 (function() {
-	'use strict'
+    'use strict'
 
-	var request = require('request');
-	var $ = require('cheerio');
-	var json = require('jsonFile');
-	var dataFile = './data.json';
-	var twilioConfig = require('./twilio.config.dev.js');
+    var request = require('request');
+    var $ = require('cheerio');
+    var json = require('jsonFile');
+    var twilioConfig = require('./twilio.config.dev.js');
 
-	var pollMin = 15000; // 15 sec
-	var pollMax = 20000; // 20 sec
+    var pollMin = 15000; // 15 sec
+    var pollMax = 20000; // 20 sec
 
-	function sendAlert(address) {
-		var twilio = require('twilio');
-		var client = new twilio.RestClient(twilioConfig.accountSid, twilioConfig.authToken);
+    var queries = [{
+        label: 'realtor',
+        url: 'http://www.realtor.com/realestateandhomes-search/Edmond_OK/beds-3/baths-2/price-125000-150000/sqft-6',
+        getLatestAddress: function(body) {
+            return $(body).find('.js-record-user-activity').first().find('.listing-street-address').text();
+        }
+    }, {
+        label: 'brandonclapp',
+        url: 'http://brandonclapp.com',
+        getLatestAddress: function(body) {
+            return $(body).find('title').text();
+        }
+    }];
 
-		client.messages.create({
-		    to: twilioConfig.toPhoneNum,
-		    from: twilioConfig.twilioPhoneNum,
-		    body: address + ' just got posted!'
-		}, function(error, message) {
-		    if (error) {
-		        console.log(error.message);
-		    }
-		});
-	}
+    function sendAlert(address) {
+        var twilio = require('twilio');
+        var client = new twilio.RestClient(twilioConfig.accountSid, twilioConfig.authToken);
 
-	function runRequest(counter) {
-		console.log('counter coming in', counter);
+        client.messages.create({
+            to: twilioConfig.toPhoneNum,
+            from: twilioConfig.twilioPhoneNum,
+            body: address + ' just got posted!'
+        }, function(error, message) {
+            if (error) {
+                console.log(error.message);
+            }
+        });
+    }
 
-		var data = json.readFileSync(dataFile);
-		console.log('reading json file', data);
+    function runRequest(counter, query) {
 
-		var query = 'http://www.realtor.com/realestateandhomes-search/Edmond_OK/beds-3/baths-2/price-125000-150000/sqft-6';
-		//var query = 'http://brandonclapp.com';
+        var dataFile = './data/' + query.label + '.json';
+        var data = json.readFileSync(dataFile);
+        console.log('reading ' + dataFile + ' json file', data);
 
-		request(query, function(error, response, body) {
+        request(query.url, function(error, response, body) {
 
-			var latestAddress = $(body).find('.js-record-user-activity').first().find('.listing-street-address').text();
-			//var latestAddress = $(body).find('title').text();
+            var latestAddress = query.getLatestAddress(body);
 
-			if(counter) {
-				console.log(counter + ' time around...');
-				// compare it with the latestAddress in data store
-				if(data.latestAddress !== latestAddress) {
-					console.log('its new, send text');
-					sendAlert(latestAddress);
-				}
-			}
+            if (counter) {
+                console.log(counter + ' time around for ' + query.label);
+                // compare it with the latestAddress in data store
+                if (data.latestAddress !== latestAddress) {
+                    console.log('its new, send text');
+                    sendAlert(latestAddress);
+                }
+            }
 
-			json.writeFileSync(dataFile, { "latestAddress": latestAddress });
+            json.writeFileSync(dataFile, {
+                "latestAddress": latestAddress
+            });
+            console.log(query.label + ' HTTP request & data store updated');
+        });
+    }
 
-			counter++;
-		});
-	}
+    function init() {
+        var counter = 0;
+        var poll = function() {
 
-	function init() {
-		var counter = 0;
-	    var poll = function() {
+            for (var i = 0; i < queries.length; i++) {
+                runRequest(counter, queries[i]);
+            }
 
-	        runRequest(counter);
-			counter++;
-	        var rand = Math.round(Math.random() * (pollMax - pollMin)) + pollMin; // generate new time between min and max
-	        setTimeout(poll, rand);
-	    }
+            counter++;
+            var rand = Math.round(Math.random() * (pollMax - pollMin)) + pollMin; // generate new time between min and max
+            setTimeout(poll, rand);
+        }
 
-    	poll();
-	}
+        poll();
+    }
 
-	init();
+    init();
 
 })();
